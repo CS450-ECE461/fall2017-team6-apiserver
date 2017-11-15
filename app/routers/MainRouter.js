4d7f7e44f3dda6b7e0513167e007bd57a1e4d21e
corylehnert <lehnertcory@gmail.com>
2017-11-15 02:25:00 -0500

    router for database

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -1,5 +1,13 @@
 module.exports = exports = {
-  '/account/login': {
-    post: {action: 'LoginController@login'},
-  },
+  '/dashboard' : { //should get the account
+    resource: {
+      controller: "AccountController"
+    }
+  }
+ 
+  '/create' : {
+    resource: {
+      controller: "AccountController"
+    }
+  }
 };
