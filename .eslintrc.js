669405d51ad58c03e023fd21b8e279862bb9def2
Alex Hicks <aldahick@iupui.edu>
2017-11-26 13:36:34 -0500

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,16 @@ module.exports = {
     "extends": "google",
     "parserOptions": {
       "ecmaVersion": 6
+    },
+    "rules": {
+      "require-jsdoc": ["error", {
+        "require": {
+          "FunctionDeclaration": false,
+          "MethodDefinition": false,
+          "ClassDeclaration": false,
+          "ArrowFunctionExpression": false,
+          "FunctionExpression": false
+        }
+      }]
     }
 };
