f8de241ef8705a19d4b4d0d4a3ac16ceb5f456dd
corylehnert <lehnertcory@gmail.com>
2017-11-26 13:41:41 -0500

    updated MainRouter
    Made it use User and LoginController as well as added DiagnosticController

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -1,19 +1,19 @@
-smodule.exports = exports = {
-  '/dashboard' : { //should get the account
+module.exports = exports = {
+  '/account' : { //should get the account
     resource: {
-      controller: "AccountController"
-    }
-  }
- 
-  '/create' : {
-    resource: {
-      controller: "AccountController"
+      controller: "UserController"
     }
   }
 
-  '/mechanic' : {
+  '/mechanic' : { //for accessing the mechanic's info page
    resource: {
      controller: "MechanicController"
+    }
+  }
+
+ '/diagnostic' : { // for accessing diagnostic requests
+   resource: {
+    controller: "DiagnosticController"
     }
   }
 };
