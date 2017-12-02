311eac865992a05ae782bc67f75e0e820f81f867
clehnert <clehnert@umail.iu.edu>
2017-12-01 20:10:19 -0500

    Cleaned up naming errors

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -1,15 +1,15 @@
 module.exports = exports = {
   '/account' : { //should get the account
     resource: {
-      controller: "UserController"
+      controller: "LoginController"
     }
-  }
+  },
 
   '/mechanic' : { //for accessing the mechanic's info page
    resource: {
      controller: "MechanicController"
     }
-  }
+  },
 
  '/diagnostic' : { // for accessing diagnostic requests
    resource: {
